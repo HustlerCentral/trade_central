b63b19a47697618354ba6ab1a2f654bf09b25f6d
Prakash Shekhar <prakashshkhr@gmail.com>
2022-07-30 21:54:58 -0400

    update inventory

--- src/Tabs/Inventory.js
+++ src/Tabs/Inventory.js
@@ -1,14 +1,17 @@
 import fetch from "node-fetch";
 
 
-async function fetchText() {
+async function getJson() {
   let response = await fetch("https://prayola.myshopify.com/admin/api/2020-07/products.json", {
   headers: {
     'Content-Type': 'application/json',
     'X-Shopify-Access-Token': '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'
   }});
   let data = await response.json();
-  console.log(data["products"]);
+  for (let index = 0; index < data["products"].length; index++) {
+    const element = data["products"][index];
+    console.log(element);
+  }
 }
 
 fetchText();
